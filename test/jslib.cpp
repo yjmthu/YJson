6c32dd679ce0fedcec4d272498c61807ec5f69ab
Megix Cool <yjmthu@gmail.com>
2023-08-10 20:02:07 +0800

    添加动态库测试

---
+++ test/jslib.cpp
@@ -0,0 +1,10 @@
+﻿#include <vector>
+#include <yjson.h>
+
+std::vector<int> js2array(const YJson& json) {
+  std::vector<int> result;
+  for (auto& i: json.getArray()) {
+    result.push_back(i.getValueInt());
+  }
+  return result;
+}
